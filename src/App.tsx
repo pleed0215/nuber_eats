100fe596694fadc00aab468b0d1225c8e701443f
Lee Eun Deok <pleed0215@gmail.com>
2020-12-07 17:17:12 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="my_class bg-gradient-to-br from-red-500 shadow-2xl flex justify-center items-center">
+        <h1 className="text-3xl text-black text-center">Hello tailwind</h1>
+      </div>
     </div>
   );
 }
