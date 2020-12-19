51d3e252bcb26e7b6c79127906fa76da9d4c768c
이은덕 <pleed0215@ieundeog-ui-Macmini.local>
2020-12-19 12:49:01 +0900

    remains

--- src/pages/restaurant/my.restaurant.tsx
+++ src/pages/restaurant/my.restaurant.tsx
@@ -1,5 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { QueryMyRestaurants } from "../../codegen/QueryMyRestaurants";
+import { RESTAURANT_FRAGMENT } from "../../fragments";
+
+const GQL_MYRESTAURANTS = gql`
+  query QueryMyRestaurants {
+    myRestaurants {
+      ok
+      error
+      count
+      restaurants {
+        ...RestaurantPart
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
 
 export const MyRestaurant = () => {
-  return <div>My Restaurant</div>;
+  const { data, loading } = useQuery<QueryMyRestaurants>(GQL_MYRESTAURANTS);
+
+  return (
+    <div>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        data?.myRestaurants?.restaurants?.map((r) => (
+          <div key={r.id}>{r.name}</div>
+        ))
+      )}
+    </div>
+  );
 };
