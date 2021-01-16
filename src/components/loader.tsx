64c433a011f05c63762128cda93be58b44307f6c
Lee Eun Deok <pleed0215@gmail.com>
2021-01-16 12:08:21 +0900

    jj

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,16 +1,35 @@
 import React from "react";
 
+enum SpinType {
+  default = "default",
+  tailSpin = "tail-spin",
+}
+
+export type Spin = keyof typeof SpinType;
+
 // color tailwindcss color
 interface ILoader {
   size?: number; // size using tailwindcss unit.
   color?: string; // web color not tailwind color
+  type?: Spin;
 }
 
-export const Loader: React.FC<ILoader> = ({ size = 5, color = "#44a8db" }) => {
+export const Loader: React.FC<ILoader> = ({
+  size = 5,
+  color = "#44a8db",
+  type = "default",
+}) => {
   return (
-    <div
-      className={`animate-spin w-${size} h-${size} rounded-full border-2 border-gray-200`}
-      style={{ borderTopColor: color }}
-    ></div>
+    <>
+      {type === "default" && (
+        <div
+          className={`animate-spin w-${size} h-${size} rounded-full border-2 border-gray-200`}
+          style={{ borderTopColor: color }}
+        />
+      )}
+      {type === "tailSpin" && (
+        <img src={process.env.PUBLIC_URL + "tail-spin.svg"} />
+      )}
+    </>
   );
 };
